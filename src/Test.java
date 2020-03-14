5c657108f9f0aa8053edbb3a0a1a6970361d083e
jaejin0209 <jaejin0209@naver.com>
2020-03-14 22:31:29 +0900

    20200314

--- src/Test.java
+++ src/Test.java
@@ -1,17 +1,4 @@
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.util.Scanner;
 
 public class Test {
-	public static void main(String[] args) throws IOException {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		sc.close();
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		for (int i = 1; i <= n; i++) {
-			bw.write(i + "\n");
-		}
-		bw.flush();
-	}
+    
 }
--- src/Testcase.java
+++ src/Testcase.java
@@ -1,22 +1,4 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Random;
 
 public class Testcase {
-	public static void main(String[] args) throws IOException {
-		BufferedWriter bw = new BufferedWriter(new FileWriter("Test.in"));
-		bw.write("20000 300000\n1\n");
-		Random random = new Random();
-		for (int i = 0; i < 300000; i++) {
-			int u = Math.abs(random.nextInt()) % 20000+1;
-			int v = Math.abs(random.nextInt()) % 20000+1;
-			while (u == v) {
-				v = Math.abs(random.nextInt()) % 20000+1;
-			}
-			int w = Math.abs(random.nextInt()) % 10+1;
-			bw.write(u + " " + v + " " + w + "\n");
-		}
-		bw.flush();
-	}
+
 }
